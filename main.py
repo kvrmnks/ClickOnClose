34ee917be03eecfb8d39d3ee910685d3fcbd0b0b
kvrmnks <tayyx2000@163.com>
2022-04-25 18:52:57 +0800

    fix(hotkey)

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import pyautogui
 import win32gui
 import time
-TIME_INTERVAL = 5
+TIME_INTERVAL = 5 # s
 '''
 pip install pywin32
 pip install pyautogui
 '''
+hot_key = ['ctrl', 'alt', 'w']
 if __name__ == '__main__':
-    pyautogui.hotkey('ctrl','alt','w')
-    exit(0)
+
     x = int(input('input the handle(get by spy++)\n'), 16)
     while True:
         # hwnd = win32gui.FindWindow(None,'腾讯会议')
         # print(win32gui.IsWindow(x))
         flag = win32gui.IsWindow(x)
         if flag == 0:
-            print("abab")
+            for x in hot_key:
+                pyautogui.keyDown(x)
+            for x in hot_key:
+                pyautogui.keyUp(x)
             break
             
         time.sleep(TIME_INTERVAL)
